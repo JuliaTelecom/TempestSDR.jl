9f749a19bfc4c4d089d259f3fe503163f7df46ca
Robin Gerzaguet <robin.gerzaguet@irisa.fr>
2023-01-17 15:33:27 +0100

    Update stopping mode for multi-proc

--- production/test_mp.jl
+++ production/test_mp.jl
@@ -27,7 +27,7 @@ function start_runtime(duration)
     # --- SDR parameters 
     # ---------------------------------------------------- 
     carrierFreq  = 764e6
-    samplingRate = 8e6
+    samplingRate = 20e6
     gain         = 20 
     acquisition   = 0.05
     nbS = Int( 80 * 99900)
@@ -35,14 +35,14 @@ function start_runtime(duration)
     # ----------------------------------------------------
     # --- Remote SDR call 
     # ---------------------------------------------------- 
-    global channel = RemoteChannel(()->Channel{Vector{ComplexF32}}(1), PID_SDR)
+    global channel = RemoteChannel(()->Channel{Vector{ComplexF32}}(2), PID_SDR)
     future_prod = @spawnat PID_SDR start_remote_sdr(channel,nbS,:radiosim,carrierFreq,buffer=sigRx,bufferSize=nbS,samplingRate,gain;depth=4,addr="usb:0.4.5",packetSize=nbS)
 
 
     # ----------------------------------------------------
     # --- Instantiate radio 
     # ---------------------------------------------------- 
-    runtime = init_tempestSDR_runtime(channel,nbS,:makie)
+    runtime = init_tempestSDR_runtime(channel,nbS,:terminal)
     # ----------------------------------------------------
     # --- Start radio thread 
     # ---------------------------------------------------- 
@@ -66,10 +66,12 @@ function start_runtime(duration)
     run(`clear`)
     @info "Stopping all threads"
     # Stopping SDR call 
-    remote_do(stop_remote_sdr,PID_SDR)
-    sleep(0.1)
     # Stopping other calls
     stop_processing()
+    sleep(1.0)
+    remote_do(stop_remote_sdr,PID_SDR)
+    # Ensure producer stops 
+    @async Base.throwto(tup.task_producer,InterruptException())
 
     return (;runtime,task_producer,task_consummer,task_rendering,future_prod)
 end
