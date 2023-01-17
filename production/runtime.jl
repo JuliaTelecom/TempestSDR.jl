65ecbbc13f753c82e960b5b75fe359a0f828ffd2
Robin Gerzaguet <robin.gerzaguet@irisa.fr>
2023-01-17 16:10:20 +0100

    New tuntime with multiproc and real time management

--- production/runtime.jl
+++ production/runtime.jl
@@ -1,69 +1,81 @@
-
 # ----------------------------------------------------
 # --- Multi process environnement 
 # ---------------------------------------------------- 
 include("../setMP.jl")
 
-try 
-    if IS_LOADED == false 
-        rethrow(InterruptException())
-    end
-catch exception 
-    @info "Loading data"
-    #local completePath = "$(pwd())/$DATA_PATH/testX310.dat"
-    local completePath = "/Users/Robin/data_tempest/testX310.dat"
-    global sigRx = readComplexBinary(completePath,:single)
-    #global sigId = sigRx
-    global DUMP = sigRx
-    global IS_LOADED = true 
-end
 
 # ----------------------------------------------------
-# --- Init runtime 
+# --- Core processing arguments 
 # ---------------------------------------------------- 
-function start_runtime()
+@everywhere begin 
+    try 
+        if IS_LOADED == false 
+            rethrow(InterruptException())
+        end
+    catch exception 
+        @info "Loading data"
+        #local completePath = "$(pwd())/$DATA_PATH/testX310.dat"
+        local completePath = "/Users/Robin/data_tempest/testX310.dat"
+        global sigRx = readComplexBinary(completePath,:single)
+        #global sigId = sigRx
+        global DUMP = sigRx
+        global IS_LOADED = true 
+    end
+end
+PID_SDR = TempestSDR.PID_SDR
+
+
+
+function start_runtime(duration)
     # ----------------------------------------------------
     # --- SDR parameters 
     # ---------------------------------------------------- 
     carrierFreq  = 764e6
-    samplingRate = 8e6
+    samplingRate = 20e6
     gain         = 20 
-    acquisition   = 0.5 
+    acquisition   = 0.05
+    nbS = Int( 80 * 99900)
+
+    # ----------------------------------------------------
+    # --- Remote SDR call 
+    # ---------------------------------------------------- 
+    global channel = RemoteChannel(()->Channel{Vector{ComplexF32}}(2), PID_SDR)
+    future_prod = @spawnat PID_SDR start_remote_sdr(channel,nbS,:radiosim,carrierFreq,buffer=sigRx,bufferSize=nbS,samplingRate,gain;depth=4,addr="usb:0.4.5",packetSize=nbS)
+
+
     # ----------------------------------------------------
     # --- Instantiate radio 
     # ---------------------------------------------------- 
-    nbS = Int( samplingRate * acquisition)
-    global runtime = init_tempestSDR_runtime(:pluto,carrierFreq,samplingRate,gain;addr="usb:0.4.5",depth=4,buffer=sigRx,bufferSize=nbS,packetSize=nbS,renderer=:makie)
+    runtime = init_tempestSDR_runtime(channel,nbS,:makie)
     # ----------------------------------------------------
     # --- Start radio thread 
     # ---------------------------------------------------- 
     task_producer = @async circ_producer(runtime.csdr) 
+
     # ----------------------------------------------------
     # --- First extract autocorrelation properties 
     # ---------------------------------------------------- 
-    #extract_configuration(runtime)
+    extract_configuration(runtime)
     # ----------------------------------------------------
     # --- Launching image generation 
     # ---------------------------------------------------- 
-    #task_rendering  = @async image_rendering(runtime)
-    #task_consummer  = @async coreProcessing(runtime)
-    # ----------------------------------------------------
-    # --- Image rendering 
-    # ---------------------------------------------------- 
-    #try 
-    #image_rendering(runtime)
-    #catch exception 
-    #@info "Interruption" 
-    #println(exception)
-    #end
-    #return (;runtime,task_producer,task_consummer,task_rendering)
-    return (;runtime,task_producer)
-end
+    task_rendering  = @async image_rendering(runtime)
+    task_consummer  = @async coreProcessing(runtime)
 
-# ----------------------------------------------------
-# --- Stopping runtime 
-# ---------------------------------------------------- 
-#(runtime, task_producer,task_consummer,task_rendering) = start_runtime()
-tup = start_runtime()
-sleep(20)
-stop_processing(tup.runtime)
+
+    # ----------------------------------------------------
+    # --- Stopping threads
+    # ---------------------------------------------------- 
+    sleep(duration) 
+    run(`clear`)
+    @info "Stopping all threads"
+    # Stopping SDR call 
+    # Stopping other calls
+    stop_processing()
+    sleep(1.0)
+    remote_do(stop_remote_sdr,PID_SDR)
+    # Ensure producer stops 
+    @async Base.throwto(tup.task_producer,InterruptException())
+
+    return (;runtime,task_producer,task_consummer,task_rendering,future_prod)
+end
--- setMP.jl
+++ setMP.jl
@@ -1,6 +1,19 @@
 # This file automatically configure the current project to use all the avaliable Julia workers. Be sure julia is launched with `-p x` with `x`the number of desired workers
 
 using Distributed 
+
+# ----------------------------------------------------
+# --- Switch to X procs 
+# ---------------------------------------------------- 
+const N_PROCS_REQUIRED = 2 
+if nprocs() < N_PROCS_REQUIRED 
+    addprocs( N_PROCS_REQUIRED - nprocs)
+end
+
+
+# ----------------------------------------------------
+# --- Instantiate env to all workers 
+# ---------------------------------------------------- 
 if nprocs() > 1 
     # Using package manager and worker manager 
     @everywhere using Distributed,Pkg 
