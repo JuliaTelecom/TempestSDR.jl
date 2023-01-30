f814fc168be46ed89366ea55bb6c89b2653d6f03
Robin Gerzaguet <robin.gerzaguet@irisa.fr>
2023-01-30 13:24:47 +0100

    Interruption as specific function out of main call

--- production/runtime.jl
+++ production/runtime.jl
@@ -34,9 +34,9 @@ function start_runtime(duration,device=:radiosim)
     # --- SDR parameters 
     # ---------------------------------------------------- 
     carrierFreq  = 764e6
-    samplingRate = 20e6
-    gain         = 20 
-    acquisition   = 0.50
+    samplingRate = 8e6
+    gain         = 50 
+    acquisition   = 0.10
     #nbS = Int( 80 * 99900)
     nbS = Int(round(acquisition * samplingRate))
 
@@ -50,16 +50,20 @@ function start_runtime(duration,device=:radiosim)
     if device == :radiosim 
         runtime = init_tempestSDR_runtime(:radiosim,carrierFreq,samplingRate,gain;addr="usb:1.4.5",bufferSize=nbS,buffer=sigRx,packetSize=nbS,renderer=:makie)
     else 
-        runtime = init_tempestSDR_runtime(device,carrierFreq,samplingRate,gain;addr="usb:0.10.5",bufferSize=nbS,renderer=:makie)
+    runtime = init_tempestSDR_runtime(device,carrierFreq,samplingRate,gain,bufferSize=nbS,renderer=:makie;addr="usb:0.10.5")
     end
     # ----------------------------------------------------
     # --- Start radio threads 
     # ---------------------------------------------------- 
+    print(runtime.csdr.sdr)
     task_producer = Threads.@spawn start_thread_sdr(runtime.csdr)
     # ----------------------------------------------------
     # --- First extract autocorrelation properties 
     # ---------------------------------------------------- 
     extract_configuration(runtime)
+
+    runtime.config.refresh = 60.14
+
     screen = initScreenRenderer(runtime.renderer,runtime.config.height,runtime.config.width)
 
     # ----------------------------------------------------
@@ -70,20 +74,19 @@ function start_runtime(duration,device=:radiosim)
     # Consommation in remote thread
     task_consummer  = Threads.@spawn coreProcessing(runtime)
 
+    return (;runtime,task_producer,task_consummer,task_rendering,screen)
+end
 
-    # ----------------------------------------------------
-    # --- Stopping threads
-    # ---------------------------------------------------- 
-    sleep(duration) 
+
+function stop_runtime(tup)
     @info "Stopping all threads"
     # SDR safe stop 
-    @async Base.throwto(task_producer,InterruptException())
+    @async Base.throwto(tup.task_producer,InterruptException())
     sleep(1)
     # Task stop 
-    @async Base.throwto(task_consummer,InterruptException())
-    @async Base.throwto(task_rendering,InterruptException())
+    @async Base.throwto(tup.task_consummer,InterruptException())
+    @async Base.throwto(tup.task_rendering,InterruptException())
     # Safely close radio 
-    close(runtime.csdr)
-    #stop_runtime(runtime)
-    return (;runtime,task_producer,task_consummer,task_rendering,screen)
+    close(tup.runtime.csdr)
+    return nothing
 end
